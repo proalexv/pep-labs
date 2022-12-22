8632485f0b981b80158f22d6111d83f5ce04fc46
Alejandro Rodriguez <avidleader13@gmail.com>
2022-12-22 21:57:44 +0000

    Saved progress on BooleanOperators 2022-12-22 21:57:44 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,16 +14,20 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
-        return false;
+        if (b == true) {
+           return true;
+        } 
+        else {
+            return false;    
     }
-
+    }
     /**
      * @param b a true/false value.
      * @return if b is true, return false. if b is false, return true.
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        return !b;
     }
 
     /**
