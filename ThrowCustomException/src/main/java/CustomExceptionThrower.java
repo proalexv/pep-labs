f6fe5d582746f5797fa00616346c419cb5677e42
Alejandro Rodriguez <avidleader13@gmail.com>
2023-01-05 21:07:39 +0000

    Saved progress on ThrowCustomException 2023-01-05 21:07:39 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -1,6 +1,7 @@
 
 public class CustomExceptionThrower {
 
+
     /**
      * This method should throw a CustomException. CustomException is a custom exception that we've written ourselves
      * by extending the Exception class in CustomException.java. In a large scale application, writing custom
@@ -8,8 +9,8 @@ public class CustomExceptionThrower {
      *
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
-    public void throwCustomException(){
-
+    public void throwCustomException()throws CustomException{
+        throw new CustomException();
     }
 
 }
