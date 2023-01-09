b9ae00c0efa7b1241a8b011c23e42cfb4f532e06
Alejandro Rodriguez <avidleader13@gmail.com>
2023-01-09 04:26:22 +0000

    Saved progress on Queue 2023-01-09 04:26:22 +0000

--- Queue/src/main/java/QueueExercise.java
+++ Queue/src/main/java/QueueExercise.java
@@ -2,6 +2,7 @@
  * TODO: switch to deque
  */
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -29,7 +30,9 @@ public class QueueExercise {
      * Stack behavior.
      */
     public Queue<String> createQueue(){
-        return null;
+        Queue<String> example  = new LinkedList<String>();
+        return example;
+    
     }
 
     /**
