24e130253a65fdb38ad4084184ca78d2f8bc4465
Alejandro Rodriguez <avidleader13@gmail.com>
2023-01-09 03:39:03 +0000

    Saved progress on Map 2023-01-09 03:39:03 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -1,5 +1,8 @@
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -26,7 +29,8 @@ public class MapExercise {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        HashMap<Integer, String> exampleMap = new HashMap<>();
+        return exampleMap;
     }
 
     /**
@@ -36,7 +40,7 @@ public class MapExercise {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -47,6 +51,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key,value);
     }
 
     /**
@@ -56,7 +61,8 @@ public class MapExercise {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        
+        return map.get(key);
     }
 
     /**
@@ -66,6 +72,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +83,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
+        map.put(key,value);
 
     }
 }
