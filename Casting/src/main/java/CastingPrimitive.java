3497ac8a225df57746340fbe15a2662063611702
Alejandro Rodriguez <avidleader13@gmail.com>
2023-01-11 01:54:22 +0000

    Saved progress on Casting 2023-01-11 01:54:22 +0000

--- Casting/src/main/java/CastingPrimitive.java
+++ Casting/src/main/java/CastingPrimitive.java
@@ -16,6 +16,8 @@ public class CastingPrimitive {
      * @return the int form of d.
      */
     public int castToInt(double d){
-        return 0;
+        int integer = (int)d;
+        return integer;
+        
     }
 }
