c31d80f66dd3b53e2d355379873387e455a094bf
Alejandro Rodriguez <avidleader13@gmail.com>
2023-01-29 00:33:30 +0000

    Saved progress on IndexOf 2023-01-29 00:33:30 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -12,5 +12,30 @@ public class IndexOf {
      */
     public int getIndex(int[] arr, int n){
         return -1;
+    {
+ 
+        // if array is Null
+        if (arr == null) {
+            return -1;
+        }
+ 
+        // find length of array
+        int len = arr.length;
+        int i = 0;
+ 
+        // traverse in the array
+        while (i < len) {
+ 
+            // if the i-th element is t
+            // then return the index
+            if (arr[i] == t) {
+                return i;
+            }
+            else {
+                i = i + 1;
+            }
+        }
+        return -1;
+    }
     }
 }
