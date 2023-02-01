a7700f6cf8c8510dd450034f63076a381aa5fc2f
Alejandro Rodriguez <avidleader13@gmail.com>
2023-02-01 01:54:57 +0000

    Saved progress on ArraySum 2023-02-01 01:54:57 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance = this.balance + amount;
     }
 
     /**
@@ -32,7 +32,8 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if (balance - amount >= 0) {
+        balance = balance - amount;}
     }
 
     /**
