28521a073bc53ef010194704e2aaedbfbda58e59
Alejandro Rodriguez <avidleader13@gmail.com>
2023-02-03 05:23:28 +0000

    Saved progress on Library 2023-02-03 05:23:28 +0000

--- Library/src/main/java/Application/DAO/AuthorDAO.java
+++ Library/src/main/java/Application/DAO/AuthorDAO.java
@@ -52,7 +52,7 @@ public class AuthorDAO {
         try {
 //          Write SQL logic here. You should only be inserting with the name column, so that the database may
 //          automatically generate a primary key.
-            String sql = "INSERT INTO Author (name) VALUES (Alejandro);" ;
+            String sql = "INSERT INTO Author (name) VALUES ('Alejandro');" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString method here.
--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -32,7 +32,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM book";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -109,10 +109,11 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM book WHERE copies_available >0 ";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
