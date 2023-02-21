7001b565ee4d81954009710a0ff8357216b7e126
Alejandro Rodriguez <avidleader13@gmail.com>
2023-02-21 04:21:23 +0000

    Saved progress on StringIsPalindrome 2023-02-21 04:21:23 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,20 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
+       String reversed = "";
+
+       for (int i = str.length() - 1; i>0; i--){
+        reversed = reversed + str.charAt(i);
+
+        if(str == reversed){
+            return true;}
+           
+            else if(str != reversed){
+                return false;
+            }
+        }
+
+       
         return false;
     }
 }
