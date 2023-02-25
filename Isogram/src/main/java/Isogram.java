867336b66baf3c4e769b8449b6c16a28622fc0af
Alejandro Rodriguez <avidleader13@gmail.com>
2023-02-25 01:30:30 +0000

    Saved progress on Isogram 2023-02-25 01:30:30 +0000

--- Isogram/src/main/java/Isogram.java
+++ Isogram/src/main/java/Isogram.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Isogram {
     /**
@@ -13,6 +15,12 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        Set<Character> set = new HashSet<>();
+        for (char c1 : str.toCharArray()) {
+            if (!set.add(c1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
